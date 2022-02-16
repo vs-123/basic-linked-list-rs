a41948d9f53353784d714fe405b371000269641c
Vahin Sharma <vahinsharma@gmail.com>
2022-02-16 19:02:59 +0530

    added generics and TryFrom trait

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,24 @@
 #[derive(Debug, PartialEq, Clone)]
-struct LinkedList {
-    val: u32,
-    next: Option<Box<LinkedList>>,
+struct LinkedList<T: PartialEq + Clone + std::fmt::Display> {
+    val: T,
+    next: Option<Box<LinkedList<T>>>,
 }
 
-impl LinkedList {
-    fn new(val: u32) -> Self {
+impl<T: PartialEq + Clone + std::fmt::Display> LinkedList<T> {
+    fn new(val: T) -> Self {
         Self {
             val, next: None
         }
     }
 
-    fn from_vec(vec: Vec<u32>) -> Self {
-        if vec.is_empty() {panic!("Empty vectors not supported");}
-        let mut output = Self::new(vec[0]);
-        if vec.len() == 1 {return output;}
-        output.next = Some(Box::new(Self::from_vec(vec[1..].to_vec())));
-        output
-    }
-
-    fn add(&mut self, item: u32) {
+    fn add(&mut self, item: T) {
         match self.next {
             None => self.next = Some(Box::new(Self::new(item))),
             Some(ref mut next) => next.add(item),
         }
     }
 
-    fn remove_item(&mut self, item: u32) {
+    fn remove_item(&mut self, item: T) {
         if self.val != item {
             match self.next {
                 None => {}
@@ -37,7 +29,7 @@ impl LinkedList {
         }
     }
 
-    fn contains(&self, item: u32) -> bool {
+    fn contains(&self, item: T) -> bool {
         if self.val != item {
             match self.next {
                 None => false,
@@ -49,15 +41,26 @@ impl LinkedList {
     }
 }
 
-impl std::fmt::Display for LinkedList {
+impl<T: PartialEq + Clone + std::fmt::Display> std::fmt::Display for LinkedList<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{} {}", self.val, {if self.next.is_none() {String::new()} else {format!("-> {}", (*self.next.as_ref().unwrap().clone())).trim().to_string()}})
     }
 }
 
+impl<T: PartialEq + Clone + std::fmt::Display> TryFrom<Vec<T>> for LinkedList<T> {
+    type Error = &'static str;
+
+    fn try_from(vec: Vec<T>) -> Result<Self, Self::Error> {
+        if vec.is_empty() {return Err("Empty vectors not supported");}
+        let mut output = Self::new(vec[0].clone());
+        if vec.len() != 1 {output.next = Some(Box::new(LinkedList::try_from(vec[1..].to_vec()).unwrap()));}
+        Ok(output)
+    }
+}
+
 #[test]
 fn test() {
-    let mut ll = LinkedList::from_vec(vec![1,2,3]);
+    let mut ll = LinkedList::try_from(vec![1,2,3]).unwrap();
     ll.add(4);
     ll.add(5);
     ll.add(6);
